e7340ed7c6991df83768095078242e13f2b2ab20
Otto Gutierrez <ogutierrezgz.94@hotmail.com>
2021-07-23 16:25:56 -0500

    RESPONSIVE added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import Header from './components/Header';
 import Formulario from './components/Formulario';
 import Pdf from './components/Pdf';
@@ -7,8 +7,16 @@ import { Provider } from 'react-redux';
 import store from './store';
 
 function App() {
+	useEffect(() => {
+		if (window.screen.width <= 1277) {
+			alert(
+				'Para mayor experiencia use un dispositivo que sea mayor a 1277px',
+			);
+		}
+	}, []);
 	return (
 		<Fragment>
+			{/* <p>{sistema ? 'true' : 'false'}</p> */}
 			<Provider store={store}>
 				<Header />
 				<div className='containerMain'>
